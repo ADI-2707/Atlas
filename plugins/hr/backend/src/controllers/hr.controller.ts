6eb61d1ed0cc6691aa316d85ccb6c264c299c0b7
Aditya Singh <aditya21singh2707@gmail.com>
2026-06-30 16:16:38 +0530

    feat(hr): add hr service and controller

---
+++ plugins/hr/backend/src/controllers/hr.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, Patch, Delete, Body, Param, Query, Request } from '@nestjs/common';
+import { HrService } from '../services/hr.service';
+import { SetMetadata } from '@nestjs/common';
+
+export const RequirePermissions = (...permissions: string[]) => SetMetadata('permissions', permissions);
+
+@Controller('hr')
+export class HrController {
+  constructor(private readonly hrService: HrService) {}
+
+  @Get('employees')
+  @RequirePermissions('hr.read')
+  async getEmployees(@Request() req: any, @Query() query: any) {
+    return this.hrService.getEmployees(req.user.organizationId, query);
+  }
+
+  @Post('employees')
+  @RequirePermissions('hr.create')
+  async createEmployee(@Request() req: any, @Body() body: any) {
+    return this.hrService.createEmployee(req.user.organizationId, body, req.user.id);
+  }
+
+  @Patch('employees/:id')
+  @RequirePermissions('hr.update')
+  async updateEmployee(@Request() req: any, @Param('id') id: string, @Body() body: any) {
+    return this.hrService.updateEmployee(req.user.organizationId, id, body, req.user.id);
+  }
+
+  @Delete('employees/:id')
+  @RequirePermissions('hr.delete')
+  async deleteEmployee(@Request() req: any, @Param('id') id: string) {
+    return this.hrService.deleteEmployee(req.user.organizationId, id, req.user.id);
+  }
+
+  @Get('payroll')
+  @RequirePermissions('hr.payroll.read')
+  async getPayrollRecords(@Request() req: any, @Query() query: any) {
+    return this.hrService.getPayrollRecords(req.user.organizationId, query);
+  }
+
+  @Post('payroll')
+  @RequirePermissions('hr.payroll.write')
+  async createPayrollRecord(@Request() req: any, @Body() body: any) {
+    return this.hrService.createPayrollRecord(req.user.organizationId, body, req.user.id);
+  }
+}
